1f190f7e61942bd402b730a0a40b30c1d846fd97
黄应飞 <565267598@qq.com>
2020-08-18 11:14:42 +0800

    add 多项式回归;

--- ihyf/多项式回归/polynomial_regression.py
+++ ihyf/多项式回归/polynomial_regression.py
@@ -17,17 +17,58 @@ class PolynomialRegression(object):
     @staticmethod
     def data_preprocessing_template():
         # Importing the dataset
-        dataset = pd.read_csv('50_Startups.csv')
-        x = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, 4].values
+        dataset = pd.read_csv('Position_Salaries.csv')
+        x = dataset.iloc[:, 1:2].values
+        y = dataset.iloc[:, 2].values
+        return x, y
 
-        label_encoder = LabelEncoder()
-        x[:, 3] = label_encoder.fit_transform(x[:, 3])
-        ct = ColumnTransformer([("Country", OneHotEncoder(), [3])], remainder='passthrough')
-        x = ct.fit_transform(x)
-        # 虚拟变量陷阱
-        x = x[:, 1:]
+    def polynomial_regression(self):
+        # 用线性回归模型拟合
+        from sklearn.linear_model import LinearRegression
+        x, y = self.data_preprocessing_template()
+        lin_regress = LinearRegression()
+        lin_regress.fit(x, y)
+        # 用多项式回归拟合
+        from sklearn.preprocessing import PolynomialFeatures
+        poly_regress = PolynomialFeatures(degree=4)
+        x_poly = poly_regress.fit_transform(x)
+        lin_regress_2 = LinearRegression()
+        lin_regress_2.fit(x_poly, y)
 
-        # Splitting the dataset into the Training set and Test set
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-        return x_train, x_test, y_train, y_test
+        # 线性回归拟合结果展示
+        plt.scatter(x, y, c='r')
+        plt.plot(x, lin_regress.predict(x), c='b')
+        plt.title('Truth or Bluff (Linear Regression)')
+        plt.xlabel('Position Level')
+        plt.ylabel('Salary')
+        plt.show()
+        # 多项式回归拟合结果展示
+        x_grid = np.arange(min(x), max(x), 0.1)
+        x_grid = x_grid.reshape(len(x_grid), 1)
+        plt.scatter(x, y, c='r')
+        plt.plot(x, lin_regress.predict(x), c='b')
+        plt.plot(x_grid, lin_regress_2.predict(poly_regress.fit_transform(x_grid)), c='b')
+        plt.title('Truth or Bluff (Polynomial Regression)')
+        plt.xlabel('Position Level')
+        plt.ylabel('Salary')
+        plt.show()
+        # 预测新人的薪水
+        new_person_level = 6.5
+        new_person_level = np.array(new_person_level)
+        new_person_level = new_person_level.reshape(1, 1)
+        # 两种模型预测的新人薪水
+        print(lin_regress.predict(new_person_level)[-1])
+        new_person_salary = lin_regress_2.predict(poly_regress.fit_transform(new_person_level))
+        print(new_person_salary[-1])
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.pr = PolynomialRegression()
+
+    def test_data_preprocessing_template(self):
+        self.pr.data_preprocessing_template()
+
+    def test_polynomial_regression(self):
+        self.pr.polynomial_regression()
+
